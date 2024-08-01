d7fc572fdbec74d884bd94753d41f7108fc9b87f
TheLeoP <eugenio2305@hotmail.com>
2024-08-01 08:53:44 -0500

    fix(powershell): remove dap-powershell
    powershell.nvim already handles debugging

--- after/plugin/dapui.lua
+++ after/plugin/dapui.lua
@@ -3,5 +3,4 @@ local dap = require("dap")
 dapui.setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open({})
-    require('dap-powershell').correct_repl_colors()
 end
